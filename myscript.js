259659c4f3e62db13ef3a9bd957b66b3ecddb839
rogiertenhacken <rogiertenhacken@gmail.com>
2018-03-21 14:13:04 +0100

    added-lightbox

--- myscript.js
+++ myscript.js
@@ -5,8 +5,27 @@ for (let i = 0; i < 20; i++) {
   //div.setAttribute("id","R"+i);
   div.setAttribute("class", "card");
   img.setAttribute("class", "card-img-top");
+  img.setAttribute("onclick", "openModal();");
+  img.onclick = function() {openModal();};
   div.innerHTML = "&nbsp;"; //optional to prevent collapse
-  document.getElementById("main").appendChild(div).appendChild(img).src = "image/3x2bl.jpg"; //write div to page
+  document.getElementById("main").appendChild(div).appendChild(img).src = "image/roadBlack.jpg"; //write div to page
   };
 
+  // Open the Modal
+function openModal() {
+  document.getElementById('myModal').style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById('myModal').style.display = "none";
+}
+
+// create an auto dev-creater for the modal / lightbox
+
 // how to set the src for different pictures?! (line9)
+
+// add a onclick to img so it can be shown in a lightbox or:
+
+//https://www.w3schools.com/howto/howto_css_image_grid_responsive.asp
+//https://css-tricks.com/seamless-responsive-photo-grid/
